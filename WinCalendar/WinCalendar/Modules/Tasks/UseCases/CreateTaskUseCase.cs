e67b66b19c9eb5964d482ca1d6232e5b45a40b72
kannoqwe <pyntoxfejk@gmail.com>
2026-03-31 17:45:27 +0200

    feat: add task usecases

---
+++ WinCalendar/WinCalendar/Modules/Tasks/UseCases/CreateTaskUseCase.cs
@@ -0,0 +1,25 @@
+﻿using Planner.App.Modules.Tasks.Contracts;
+using Planner.App.Modules.Tasks.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace Planner.App.Modules.Tasks.UseCases;
+
+public sealed class CreateTaskUseCase
+{
+    private readonly ITaskRepository _taskRepository;
+
+    public CreateTaskUseCase(ITaskRepository taskRepository)
+    {
+        _taskRepository = taskRepository;
+    }
+
+    public async Task<TaskItem> ExecuteAsync(string title, DateOnly date, TimeOnly? time = null)
+    {
+        TaskItem task = new TaskItem(title, date, time);
+
+        await _taskRepository.AddAsync(task);
+
+        return task;
+    }
+}
---
+++ WinCalendar/WinCalendar/Modules/Tasks/UseCases/GetTasksForDayUseCase.cs
@@ -0,0 +1,22 @@
+﻿using Planner.App.Modules.Tasks.Contracts;
+using Planner.App.Modules.Tasks.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Planner.App.Modules.Tasks.UseCases;
+
+public sealed class GetTasksForDayUseCase
+{
+    private readonly ITaskRepository _taskRepository;
+
+    public GetTasksForDayUseCase(ITaskRepository taskRepository)
+    {
+        _taskRepository = taskRepository;
+    }
+
+    public Task<IReadOnlyList<TaskItem>> ExecuteAsync(DateOnly date)
+    {
+        return _taskRepository.GetByDateAsync(date);
+    }
+}

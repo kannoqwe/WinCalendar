03197a9a57814f237ed373ed0cf7b9e15e36bc6f
kannoqwe <pyntoxfejk@gmail.com>
2026-04-06 21:48:08 +0200

    fix(shell): use native caption icons styling

--- WinCalendar/WinCalendar/MainWindow.xaml.cs
+++ WinCalendar/WinCalendar/MainWindow.xaml.cs
@@ -18,7 +18,6 @@ namespace WinCalendar
         private readonly PlannerWindowCoordinator _windowCoordinator;
         private AppWindow? _appWindow;
         private FrameworkElement? _titleBarDragRegion;
-        private FontIcon? _captionMaximizeIcon;
 
         public MainWindow(PlannerStateStore plannerStateStore, PlannerWindowCoordinator windowCoordinator)
         {
@@ -26,11 +25,9 @@ namespace WinCalendar
             _windowCoordinator = windowCoordinator;
             InitializeComponent();
             _titleBarDragRegion = ContentRoot.FindName("TitleBarDragRegion") as FrameworkElement;
-            _captionMaximizeIcon = ContentRoot.FindName("CaptionMaximizeIcon") as FontIcon;
             ConfigureCustomTitleBar();
             ShellNavigationView.SelectedItem = TodayNavigationItem;
             PageHost.Content = new TodayPage(_plannerStateStore, _windowCoordinator);
-            Closed += MainWindow_Closed;
         }
 
         private void ShellNavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
@@ -68,43 +65,17 @@ namespace WinCalendar
                 return;
 
             AppWindowTitleBar titleBar = _appWindow.TitleBar;
-            _appWindow.Changed += AppWindow_Changed;
             titleBar.ExtendsContentIntoTitleBar = true;
             titleBar.IconShowOptions = IconShowOptions.HideIconAndSystemMenu;
             titleBar.ButtonBackgroundColor = Colors.Transparent;
             titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
             titleBar.ButtonHoverBackgroundColor = ColorHelper.FromArgb(0x14, 0x00, 0x00, 0x00);
             titleBar.ButtonPressedBackgroundColor = ColorHelper.FromArgb(0x22, 0x00, 0x00, 0x00);
-            titleBar.ButtonForegroundColor = Colors.Transparent;
-            titleBar.ButtonInactiveForegroundColor = Colors.Transparent;
-            titleBar.ButtonHoverForegroundColor = Colors.Transparent;
-            titleBar.ButtonPressedForegroundColor = Colors.Transparent;
+            titleBar.ButtonForegroundColor = Colors.Black;
+            titleBar.ButtonInactiveForegroundColor = Colors.Black;
+            titleBar.ButtonHoverForegroundColor = Colors.Black;
+            titleBar.ButtonPressedForegroundColor = Colors.Black;
             titleBar.PreferredHeightOption = TitleBarHeightOption.Standard;
-            UpdateCaptionButtonIcons();
-        }
-
-        private void AppWindow_Changed(AppWindow sender, AppWindowChangedEventArgs args)
-        {
-            if (!args.DidPresenterChange && !args.DidSizeChange)
-                return;
-
-            DispatcherQueue.TryEnqueue(UpdateCaptionButtonIcons);
-        }
-
-        private void UpdateCaptionButtonIcons()
-        {
-            if (_captionMaximizeIcon is null || _appWindow?.Presenter is not OverlappedPresenter presenter)
-                return;
-
-            _captionMaximizeIcon.Glyph = presenter.State == OverlappedPresenterState.Maximized
-                ? "\uE923"
-                : "\uE922";
-        }
-
-        private void MainWindow_Closed(object sender, WindowEventArgs args)
-        {
-            if (_appWindow is not null)
-                _appWindow.Changed -= AppWindow_Changed;
         }
     }
 }
